653d178743bdefa23daa2610366b614d7ce5e864
gifta <152134013+stefani-gifta@users.noreply.github.com>
2025-02-28 16:20:59 +0700

    Add files via upload

---
+++ Util/Randomizer.java
@@ -0,0 +1,22 @@
+package Util;
+
+import java.util.Random;
+
+public class Randomizer {
+	
+	static Random r = new Random();
+	
+	public static int randomInt(int min, int max) {
+		return min + r.nextInt(max-min);
+	}
+	
+	public static double random(int min, int max) {
+		return Double.valueOf(randomInt(min, max));
+	}
+
+	public static char getRandomCharFrom(String optionString) {
+		int index = r.nextInt(optionString.length());
+		return optionString.charAt(index);
+	}
+	
+}
---
+++ Util/Util.java
@@ -0,0 +1,42 @@
+package Util;
+
+import java.util.Scanner;
+
+public class Util {
+	
+	public static Scanner scan = new Scanner(System.in);
+
+	public static void PRESS_ENTER() {
+		System.out.println("\nPress ENTER to continue");
+		scan.nextLine();
+	}
+	
+	public static void CLEAR_CONSOLE() {
+		//System.out.println("CLEAR");
+		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	}
+	
+	public static void LOADING() {
+		for (int i = 0; i <= 5; i++) {
+			System.out.print("[");
+            for (int j = 0; j <= i; j++) {
+                System.out.print("===");
+            }
+            for (int j = 0; j < 5-i; j++) {
+                System.out.print("   ");
+            }
+            System.out.println("]");
+            System.out.printf("Loading... %d%%\n", i*20);
+            try {
+                Thread.sleep(1000); // 1 second
+            } catch (InterruptedException e) {
+            	System.out.println("Loading failed");
+                // e.printStackTrace();
+            }
+            Util.CLEAR_CONSOLE();
+        }
+        System.out.println("Loading complete!");
+        Util.PRESS_ENTER();
+	}
+
+}
